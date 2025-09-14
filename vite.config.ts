ca2559dd71c0d7a20adf17477e991fdbfcfda060
Delano Brown <ecogenmarket23.3@gmail.com>
2025-09-14 16:13:11 -0500

    chore: update favicon and app icons to vite.png

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: "dist" },
+  server: { port: 5173 },
+});
